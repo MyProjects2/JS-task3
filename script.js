1437acd582026e4927f5115f19ac7be4c68a94cc
Olga <olgayakimovich87@gmail.com>
2018-02-12 07:06:03 +0300

    function to calculate vowels

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 'use strict'
-    let lastName = prompt('Введите вашу фамилию');
-    while(lastName == '' || lastName == null){
-        lastName = prompt('Введите вашу фамилию');
-    } 
-    let firstName = prompt('Введите ваше имя');
-    while (firstName == '' || firstName == null){
-        firstName = prompt('Введите ваше имя');
-    }
-    let patronymic = prompt('Введите ваше отчество');
-    while (patronymic == '' || patronymic == null){
-        patronymic = prompt('Введите ваше отчество');
-    }
-    let age = parseInt(prompt('Ваш возраст:'));
-    while (isNaN(age) || age == ''){
-        age = parseInt(prompt('Ваш возраст:'));
-    }
-    let gender = confirm('Ваш пол мужской? Если так, нажмите "OK", если женский нажмите "Отмена"');
-    
-    let retireMen = 63;
-    let retireWomen = 58;
-    var answer;
-
-    if (gender == true) {
-        gender = 'мужской';
-        answer = (retireMen > age)? 'нет': 'да';
-    }
-    else {
-        gender = 'женский';
-        answer = (retireWomen > age)? 'нет': 'да';
-    }
-  
-    alert(`Ваше ФИО: ${lastName} ${firstName} ${patronymic} \n Ваш возраст в годах: ${age} \n Ваш возраст в днях: ${age*365} \n Через 5 лет вам будет: ${age+5} \n Ваш пол: ${gender} \n Вы на пенсии: ${answer}`);
+var userInput = prompt('Напишите любое слово');
+function calculateVowels(string) {
+    var arrayOfVowels = string.match( /[аеёиоуэюя]/gi );
+    var amount = arrayOfVowels.length;
+    return amount;
+}
+console.log('Количество гласных букв в слове(фразе): ' + calculateVowels(userInput));
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var array = userInput.split('');
+// for(var i = 0; i< vowels.length; i++){
+//     var pos = 0;
+//     var arr = [];
+//     while (true) {
+//         var found = array.indexOf(vowels[i], pos);
+//         if (found == -1) break;
+//         alert(found);
+//         arr.push(pos);
+//         console.log(arr);
+//         pos = found +1;
+//     }
+// }
+//console.log(array);
